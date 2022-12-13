35e803657f3e0d0d4a0781cac8a9b1ffd725c303
Tomas Pandullo <tomas.pandu@gmail.com>
2022-12-13 15:10:45 -0300

    correccion del error al inciar session

--- PFinal_coderhouse_44470/apptienda/views.py
+++ PFinal_coderhouse_44470/apptienda/views.py
@@ -6,22 +6,28 @@ from apptienda.models import *
 from applogin.models import Avatar
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-#vista de la pagina inicio
+# vista de la pagina inicio
+
+
 def vista_inicio(request):
     if request.user.is_authenticated:
-        imagen_model = Avatar.objects.filter(user=request.user.id)[0]
-        imagen_url = imagen_model.imagen.url
-    else:
-        imagen_url = ""
-    return render(request, "apptienda/inicio.html", {"imagen_url":imagen_url})
+        try:
+            imagen_model = Avatar.objects.filter(user=request.user.id)[0]
+            imagen_url = imagen_model.imagen.url
+        except:
+            imagen_url = ""
+        return render(request, "apptienda/inicio.html", {"imagen_url": imagen_url})
+
 
 def vista_tienda(request):
     productos = Productos.objects.all()
-    contexto = {"productos":productos}
+    contexto = {"productos": productos}
     return render(request, "apptienda/productos.html", contexto)
+
 
 def vista_about(request):
     return render(request, "apptienda/about.html")
+
 
 def vista_comprar_producto(request, id):
     producto = Productos.objects.get(id=id)
