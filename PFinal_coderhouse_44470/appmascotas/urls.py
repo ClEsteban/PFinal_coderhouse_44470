679e9f7367449e32408f526f87e951dc512cfcfe
MatiC26 <maticarignano3@gmail.com>
2022-12-16 21:20:52 -0300

    cambio en urls de borrar y editar
    sigue habiendo error cuando queres editar la mascota, dice "missing 1 required positional argument: 'id'"

--- PFinal_coderhouse_44470/appmascotas/urls.py
+++ PFinal_coderhouse_44470/appmascotas/urls.py
@@ -6,9 +6,9 @@ from applogin.views import *
 
 urlpatterns = [
     path('mascotas/', vista_mascotas, name="vet-mascotas"),
-    path('mascotas/borrar/<id>', mascota_borrar, name="mascota-borrar"),
-    path('mascotas/editar/<id>', mascota_editar, name="mascota-editar"), 
     path('mascotas/create', MascotaCrear.as_view(), name="mascota-create"),
+    path('mascotas/editar/<int:mascota_id/', mascota_editar, name="mascota-editar"), 
+    path('mascotas/borrar/<int:mascota_id/', mascota_borrar, name="mascota-borrar"),
     path('mascotas/update/<pk>', MascotaActualizar.as_view(), name="mascota-update"),
     path('mascotas/detail/<pk>', MascotaDetalle.as_view(), name="mascota-detail"),
     path('mascotas/list', MascotaList.as_view(), name="mascota-list"),
